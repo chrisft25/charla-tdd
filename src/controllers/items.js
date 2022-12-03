f4999f26dbf7166005e079fce3108960ff6a9075
Christopher Fuentes <fuenteschris75@gmail.com>
2022-12-03 02:44:00 -0600

    chore: fix test case

--- src/controllers/items.js
+++ src/controllers/items.js
@@ -1,6 +1,7 @@
 export function generateReport (sales) {
   if (!sales) throw new Error('Sales must be provided')
   if (!Array.isArray(sales)) throw new Error('Sales must be an array')
+  if (sales.length === 0) return 0
 
   return sales[0].quantity * sales[0].price
 }
