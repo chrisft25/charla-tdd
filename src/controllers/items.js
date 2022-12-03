0c6352727b94caabe0317e45226e6c58d967cadd
Christopher Fuentes <fuenteschris75@gmail.com>
2022-12-03 02:46:35 -0600

    chore: fix test case

--- src/controllers/items.js
+++ src/controllers/items.js
@@ -3,5 +3,11 @@ export function generateReport (sales) {
   if (!Array.isArray(sales)) throw new Error('Sales must be an array')
   if (sales.length === 0) return 0
 
-  return sales[0].quantity * sales[0].price
+  let total = 0
+
+  for (let i = 0; i < sales.length; i++) {
+    total = total + sales[i].quantity * sales[i].price
+  }
+
+  return total
 }
