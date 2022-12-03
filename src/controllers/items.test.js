0b63415df34188feb11a3568fcd44f84d2d8e4eb
Christopher Fuentes <fuenteschris75@gmail.com>
2022-12-03 02:37:47 -0600

    chore: test case for not array

--- src/controllers/items.test.js
+++ src/controllers/items.test.js
@@ -8,4 +8,12 @@ describe('Items Report', () => {
   it('should throw an error if sales list is not provided', () => {
     expect(() => generateReport()).toThrow('Sales must be provided')
   })
+
+  it('should throw an error if sales is not an array', () => {
+    // Arrange
+    const sales = Number
+
+    // Act & Assert
+    expect(() => generateReport(sales)).toThrow('Sales must be an array')
+  })
 })
