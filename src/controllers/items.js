f497a6a8aa914768b834cffd3467a6c810dde3ef
Christopher Fuentes <fuenteschris75@gmail.com>
2022-12-03 02:51:20 -0600

    chore: refactor test case

--- src/controllers/items.js
+++ src/controllers/items.js
@@ -3,11 +3,10 @@ export function generateReport (sales) {
   if (!Array.isArray(sales)) throw new Error('Sales must be an array')
   if (sales.length === 0) return 0
 
-  let total = 0
-
-  for (let i = 0; i < sales.length; i++) {
-    total = total + sales[i].quantity * sales[i].price
-  }
+  const total = sales.reduce((accum, value) => {
+    const itemFinalPrice = value.quantity * value.price
+    return accum + itemFinalPrice
+  }, 0)
 
   return total
 }
